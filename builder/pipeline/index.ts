3dfc5f85bbcbdb560cb6c057be9d56fb374fc44c
kps46d9g47-cloud <kps46d9g47@privaterelay.appleid.com>
2026-06-26 15:59:55 +0300

    Add graph publication pipeline public exports

---
+++ builder/pipeline/index.ts
@@ -0,0 +1,16 @@
+export type {
+  AppendixEntry,
+  GraphPublicationPipelineResult,
+  LayoutBlock,
+  LayoutPlan,
+  PipelineSectionSelector,
+  TOCEntry,
+  TraceabilityEntry,
+} from './types';
+
+export { AppendixBuilder } from './appendixBuilder';
+export { GraphPublicationPipeline } from './graphPublicationPipeline';
+export { LayoutPlanEngine } from './layoutPlanEngine';
+export { PublicationPlanBuilder } from './publicationPlanBuilder';
+export { TOCGenerator } from './tocGenerator';
+export { TraceabilityEngine } from './traceabilityEngine';
